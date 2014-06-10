97fdb3f82c21790c7141598ab80ea52798f2a853
leoalmig <leoalmig@gmail.com>
2014-06-10 15:40:49 -0300

    Banco de dados configurado

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table solucao_do_exercicio (
+  id                        bigint not null,
+  codigo                    varchar(255),
+  constraint pk_solucao_do_exercicio primary key (id))
+;
+
+create sequence solucao_do_exercicio_seq;
+
+
+
+
+# --- !Downs
+
+drop table if exists solucao_do_exercicio cascade;
+
+drop sequence if exists solucao_do_exercicio_seq;
+
