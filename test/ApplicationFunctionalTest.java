7933f2fbca26dd64d0abb1d8bb31ff3151040ce2
leoalmig <leoalmig@gmail.com>
2014-06-11 15:33:22 -0300

    <ConfiguraçãoProjeto>:Patricia&Ariel - Adicionando teste funcional que utiliza web browser

---
+++ test/ApplicationFunctionalTest.java
@@ -0,0 +1,24 @@
+import play.libs.F;
+import play.mvc.*;
+import org.junit.*;
+import play.test.TestBrowser;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.*;
+
+public class ApplicationFunctionalTest {
+
+    @Test
+    public void verificaSeOTituloEIgualAePractice() {
+        running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.$("title").getTexts().get(0)).isEqualTo("e-Practice");
+                //browser.$("a").click();
+                //assertThat(browser.url()).isEqualTo("http://localhost:3333/Coco");
+                //assertThat(browser.$("#title", 0).getText()).isEqualTo("Hello Coco");
+            }
+        });
+    }
+
+}
