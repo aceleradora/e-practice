d1a5c2163b399d27db905eee63f146f055c8b86c
fernando.rreggio <fernando.rreggio@gmail.com>
2014-07-09 13:36:25 -0300

    Ariel&Leonardo: Refatoração de testes

---
+++ test/unitario/model/SolucaoDoExercicioTest.java
@@ -0,0 +1,19 @@
+package unitario.model;
+
+import models.SolucaoDoExercicio;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by alunos4 on 09/07/14.
+ */
+public class SolucaoDoExercicioTest {
+
+    @Test
+    public void SolucaoNaoPodeTerCorpoVazio() {
+        SolucaoDoExercicio solucaoDoExercicio = new SolucaoDoExercicio("solucaoDoUsuario");
+        assertNotNull(solucaoDoExercicio.solucaoDoUsuario);
+    }
+    
+}
