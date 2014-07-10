430e70a5a783dd060ef301b7bf46a0a864ed2852
PatriciaDurand <durand_patricia_@hotmail.com>
2014-07-10 10:36:33 -0300

    1.2.1:Ariel&Luiz - Adicionado novas funcionalidades a TabelaDeSimbolos

--- app/models/TabelaDeSimbolos.java
+++ app/models/TabelaDeSimbolos.java
@@ -9,13 +9,31 @@ public class TabelaDeSimbolos {
         tabela = new HashMap<String, String>();
     }
 
-    public void adicionaSimbolo(String identificador, String tipo){
+    public boolean adicionaSimbolo(String identificador, String tipo){
         tabela.put(identificador, tipo);
+        return true;
     }
-
 
     public boolean verificaSeTipoCombina(String idv, String tipo) {
         if(tipo.equals(tabela.get(idv))) return true;
         else return false;
+    }
+
+
+    public boolean simboloExiste(String simbolo) {
+        String simboloDaTabela;
+
+        simboloDaTabela = tabela.get(simbolo);
+        if (simboloDaTabela != null){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public String getTipoSimbolo(String simbolo) {
+        String tipoDoSimboloNaTabela = tabela.get(simbolo);
+        return tipoDoSimboloNaTabela;
     }
 }
--- test/unitario/model/TesteTabelaDeSimbolos.java
+++ test/unitario/model/TesteTabelaDeSimbolos.java
@@ -4,6 +4,7 @@ import models.TabelaDeSimbolos;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TesteTabelaDeSimbolos {
@@ -27,5 +28,35 @@ public class TesteTabelaDeSimbolos {
 
         assertThat(resultado, is(false));
 
+    }
+
+    @Test
+    public void retornaSeIDVExisteNaTabelaDeSimbolos() throws Exception {
+        TabelaDeSimbolos tabelaDeSimbolos = new TabelaDeSimbolos();
+        tabelaDeSimbolos.adicionaSimbolo("x", "Inteiro");
+
+        boolean existe = tabelaDeSimbolos.simboloExiste("x");
+
+        assertThat(existe, is(true));
+    }
+
+    @Test
+    public void retornaFalseCasoIDVNaoExistaNaTabelaDeSimbolos() throws Exception {
+        TabelaDeSimbolos tabelaDeSimbolos = new TabelaDeSimbolos();
+        tabelaDeSimbolos.adicionaSimbolo("x", "Inteiro");
+
+        boolean existe = tabelaDeSimbolos.simboloExiste("p");
+
+        assertThat(existe, is(false));
+    }
+
+    @Test
+    public void retornaTipoDoIDVCasoOIDVEstejaNaTabelaDeSimbolos() throws Exception {
+        TabelaDeSimbolos tabelaDeSimbolos = new TabelaDeSimbolos();
+        tabelaDeSimbolos.adicionaSimbolo("x", "Inteiro");
+
+        String tipoIDV = tabelaDeSimbolos.getTipoSimbolo("x");
+        
+        assertThat(tipoIDV, is("Inteiro"));
     }
 }
