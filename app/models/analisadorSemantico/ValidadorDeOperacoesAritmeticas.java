f7f8cb0f7096644ab95aca301ec7f823519523b2
filipegorges <lipirr@hotmail.com>
2014-07-23 14:01:34 -0300

    2.1.3: Fernando & Filipe - criaçao do metodo valida com cobertura de testes

--- app/models/analisadorSemantico/ValidadorDeOperacoesAritmeticas.java
+++ app/models/analisadorSemantico/ValidadorDeOperacoesAritmeticas.java
@@ -1,6 +1,9 @@
 package models.analisadorSemantico;
 
+import models.TabelaDeSimbolos;
 import models.Validador;
+import models.analisadorLexico.IdentificadorDeToken;
+import models.analisadorLexico.Lexer;
 
 import java.util.ArrayList;
 
@@ -9,9 +12,28 @@ import java.util.ArrayList;
  */
 public class ValidadorDeOperacoesAritmeticas implements Validador{
 
+    private IdentificadorDeToken identificadorDeToken;
+    private TabelaDeSimbolos tabela;
+    private ArrayList<String>tokens;
 
-    public boolean valida(ArrayList<String> tokens) {
-        return false;
+
+    public ValidadorDeOperacoesAritmeticas(TabelaDeSimbolos tabela) {
+
+        identificadorDeToken = new IdentificadorDeToken();
+        this.tabela = tabela;
+
+    }
+
+    public boolean valida(ArrayList<String> tokens){
+        this.tokens = tokens;
+        boolean validacao = true;
+        for(int i = 0;i<tokens.size();i++){
+            if(identificadorDeToken.identifica(tokens.get(i)).equals("IDV")){
+                 validacao = tabela.simboloExiste(tokens.get(i));
+            }
+            if(validacao == false)return validacao;
+       }
+       return validacao;
     }
 
     @Override
