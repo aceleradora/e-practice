943a5684846b9754b37ca85d48e79e3f0c1a2f81
filipegorges <lipirr@hotmail.com>
2014-07-24 10:59:42 -0300

    3.1 Modelando classe Exercicio

---
+++ app/models/exercicioProposto/Exercicio.java
@@ -0,0 +1,10 @@
+package models.exercicioProposto;
+
+public class Exercicio {
+
+    int id;
+    String enunciado;
+    SolucaoDoExercicio possivelSolucao;
+
+
+}
---
+++ app/models/exercicioProposto/SolucaoDoExercicio.java
@@ -0,0 +1,7 @@
+package models.exercicioProposto;
+
+public class SolucaoDoExercicio {
+
+    int id;
+    String codigo;
+}
