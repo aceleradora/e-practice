4f3e69c0af6d82da3c47b7cd1adf24d9736f864e
Pedro <pedroguidoux@gmail.com>
2014-08-05 10:02:07 -0300

    3.1: Ariel & Carisa - Commitando codigo gerado no Dojo feito pelo Froes, sobre seletores aleatorios

--- test/unitario/model/exercicioProposto/SeletorAleatorioExercicioTest.java
+++ test/unitario/model/exercicioProposto/SeletorAleatorioExercicioTest.java
@@ -1,5 +1,69 @@
 package unitario.model.exercicioProposto;
 
+import models.exercicioProposto.Exercicio;
+import models.exercicioProposto.SeletorAleatorioExercicio;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SeletorAleatorioExercicioTest {
+    @Mock Exercicio exercicio;
+    private SeletorAleatorioExercicio seletorAleatorioExercicio;
+
+    @Before
+    public void setUp() throws Exception {
+        List <Exercicio> exerciciosQueEstariamNoBanco = new ArrayList<>();
+        Exercicio exercicio1 = new Exercicio("exercicio 1",null, false);
+        Exercicio exercicio2 = new Exercicio("exercicio 2",null, false);
+        Exercicio exercicio3 = new Exercicio("exercicio 3",null, false);
+        Exercicio exercicio4 = new Exercicio("exercicio 4",null, false);
+
+        exerciciosQueEstariamNoBanco.add(exercicio1);
+        exerciciosQueEstariamNoBanco.add(exercicio2);
+        exerciciosQueEstariamNoBanco.add(exercicio3);
+        exerciciosQueEstariamNoBanco.add(exercicio4);
+
+        when(exercicio.todosNaoResolvidos()).thenReturn(exerciciosQueEstariamNoBanco);
+
+        seletorAleatorioExercicio = new SeletorAleatorioExercicio(exercicio);
+
+    }
+
+    @Test
+    public void selecionaAleatoriamenteUmExercicio() throws Exception {
+        ArrayList<Exercicio> lista1 = new ArrayList<>();
+        ArrayList<Exercicio> lista2 = new ArrayList<>();
+
+        for(int i = 0; i<20; i++) {
+            lista1.add(seletorAleatorioExercicio.buscaExercicio());
+            lista2.add(seletorAleatorioExercicio.buscaExercicio());
+        }
+
+        assertThat(lista1.toArray(),not(equalTo(lista2.toArray())));
+
+    }
+
+    @Test
+    public void buscaExercicioNoBanco() throws Exception {
+        seletorAleatorioExercicio.buscaExercicio();
+
+        verify(exercicio).todosNaoResolvidos();
+    }
 
 }
