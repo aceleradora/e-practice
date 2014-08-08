6d6bb51b26d288c8bbee1008d313036ee8df951b
filipegorges <lipir@hotmail.com>
2014-08-08 09:40:26 -0300

    1.2.2 : Ajuste dos testes.

--- test/integracao/models/sintatico/GerenciadorSintaticoTest.java
+++ test/integracao/models/sintatico/GerenciadorSintaticoTest.java
@@ -4,29 +4,47 @@ import models.TabelaDeSimbolos;
 import models.analisadorLexico.IdentificadorDeToken;
 import models.analisadorLexico.Lexer;
 import models.analisadorSintatico.*;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class GerenciadorSintaticoTest {
-    @Test
-    public void retornaMensagemDeErroQuandoInterpretaUmaExpressaoAritmeticaComAspasComoOperador() throws Exception {
-        IdentificadorDeToken identificadorDeToken = new IdentificadorDeToken();
-        TabelaDeSimbolos tabelaDeSimbolos = new TabelaDeSimbolos();
-        ValidadorDeAtribuicao validadorDeAtribuicao = new ValidadorDeAtribuicao(identificadorDeToken);
-        ValidadorDeConcatenacaoDeStrings validadorDeConcatenacaoDeStrings = new ValidadorDeConcatenacaoDeStrings();
-        ValidadorDeDeclaracaoDeVariavel validadorDeDeclaracaoDeVariavel = new ValidadorDeDeclaracaoDeVariavel(tabelaDeSimbolos);
-        ValidadorDeOperacoesAritmeticas validadorDeOperacoesAritmeticas = new ValidadorDeOperacoesAritmeticas();
-        Lexer lexer = new Lexer();
-        GerenciadorBuilder builder = new GerenciadorBuilder();
-        GerenciadorSintatico gerenciadorSintatico = builder.com(lexer)
+
+        GerenciadorSintatico gerenciadorSintatico;
+    private IdentificadorDeToken identificadorDeToken;
+    private TabelaDeSimbolos tabelaDeSimbolos;
+    private ValidadorDeAtribuicao validadorDeAtribuicao;
+    private ValidadorDeConcatenacaoDeStrings validadorDeConcatenacaoDeStrings;
+    private ValidadorDeDeclaracaoDeVariavel validadorDeDeclaracaoDeVariavel;
+    private ValidadorDeOperacoesAritmeticas validadorDeOperacoesAritmeticas;
+    private Lexer lexer;
+    private GerenciadorBuilder builder;
+
+    @Before
+    public void setUp() throws Exception {
+
+        builder = new GerenciadorBuilder();
+        lexer = new Lexer();
+        identificadorDeToken = new IdentificadorDeToken();
+        tabelaDeSimbolos = new TabelaDeSimbolos();
+        validadorDeAtribuicao = new ValidadorDeAtribuicao(identificadorDeToken);
+        validadorDeConcatenacaoDeStrings = new ValidadorDeConcatenacaoDeStrings();
+        validadorDeDeclaracaoDeVariavel = new ValidadorDeDeclaracaoDeVariavel(tabelaDeSimbolos);
+        validadorDeOperacoesAritmeticas = new ValidadorDeOperacoesAritmeticas();
+        gerenciadorSintatico = builder.com(lexer)
                 .com(identificadorDeToken)
                 .com(validadorDeAtribuicao)
                 .com(validadorDeConcatenacaoDeStrings)
                 .com(validadorDeDeclaracaoDeVariavel)
                 .com(validadorDeOperacoesAritmeticas)
                 .geraGerenciador();
+    }
+
+    @Test
+    public void retornaMensagemDeErroQuandoInterpretaUmaExpressaoAritmeticaComAspasComoOperador() throws Exception {
 
         gerenciadorSintatico.interpreta("n = 2 \"\" 2");
         String resultado = gerenciadorSintatico.mostraMensagensDeErro();
@@ -34,5 +52,13 @@ public class GerenciadorSintaticoTest {
         assertThat(resultado, is("Existem erros na expressão aritmetica."));
     }
 
+    @Ignore
+    @Test
+    public void validaExpressaoInvalida() throws Exception {
 
+        gerenciadorSintatico.interpreta("n = a \"\" b");
+        String resultado = gerenciadorSintatico.mostraMensagensDeErro();
+
+        assertThat(resultado, is("Existem erros."));
+    }
 }
