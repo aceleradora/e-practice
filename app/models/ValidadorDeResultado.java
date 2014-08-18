888027f24dbdd42d04ade89800b19140a149b2d5
leoalmig <leoalmig@gmail.com>
2014-08-18 11:50:54 -0300

    2.2.1 Lucas & Fernando : Criando Validador De Resultado (varres)

--- app/models/ValidadorDeResultado.java
+++ app/models/ValidadorDeResultado.java
@@ -1,11 +1,18 @@
 package models;
 
+import models.analisadorLexico.IdentificadorDeToken;
 import models.analisadorLexico.Lexer;
 import models.exercicioProposto.Exercicio;
 
 import java.util.ArrayList;
 
 public class ValidadorDeResultado {
+
+    TabelaDeSimbolos tabelaDeSimbolos;
+
+    public ValidadorDeResultado(TabelaDeSimbolos tabelaDeSimbolos) {
+        this.tabelaDeSimbolos = tabelaDeSimbolos;
+    }
 
     public boolean validaResultadoDoUsuario(Exercicio solucaoReal, SolucaoDoExercicio solucaoDoExercicio) {
 
@@ -15,6 +22,17 @@ public class ValidadorDeResultado {
 
         if(resultado.get(2).equals(solucaoReal.possivelSolucao.getSolucaoDoUsuario())){
 
+            return true;
+        }
+        return false;
+    }
+
+    public boolean comparaTiposDoResultado(SolucaoDoExercicio solucaoDoUsuario, SolucaoDoExercicio possivelSolucao) {
+
+        String tipoSolucaoDoUsuario = tabelaDeSimbolos.getTipoSimbolo(solucaoDoUsuario.getSolucaoDoUsuario());
+        String tipoPossivelSolucao = tabelaDeSimbolos.getTipoSimbolo(possivelSolucao.getSolucaoDoUsuario());
+
+        if(tipoSolucaoDoUsuario.equals(tipoPossivelSolucao)) {
             return true;
         }
         return false;
--- test/unitario/model/TesteValidadorDeResultado.java
+++ test/unitario/model/TesteValidadorDeResultado.java
@@ -1,23 +1,28 @@
 package unitario.model;
 
 import models.SolucaoDoExercicio;
+import models.TabelaDeSimbolos;
 import models.ValidadorDeResultado;
 import models.exercicioProposto.Exercicio;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TesteValidadorDeResultado {
-
-
 
         SolucaoDoExercicio solucaoDoUsuario;
         SolucaoDoExercicio possivelSolucao;
         Exercicio exercicio;
         ValidadorDeResultado validadorDeResultado;
+        @Mock TabelaDeSimbolos tabelaDeSimbolos;
+
 
     @Before
     public void setUp() throws Exception {
@@ -25,7 +30,8 @@ public class TesteValidadorDeResultado {
         solucaoDoUsuario = new SolucaoDoExercicio("resultado = 2");
         possivelSolucao = new SolucaoDoExercicio("2");
         exercicio = new Exercicio("1 + 1", possivelSolucao, false);
-        validadorDeResultado = new ValidadorDeResultado();
+        validadorDeResultado = new ValidadorDeResultado(tabelaDeSimbolos);
+
 
     }
 
@@ -36,5 +42,17 @@ public class TesteValidadorDeResultado {
 
         assertThat(resultado, is(true));
 
+    }
+
+    @Test
+    public void dadoQueResultadoDoUsuarioEPossivelSolucaoSaoDeTiposDiferentesRetornaFalse() throws Exception {
+
+        solucaoDoUsuario = new SolucaoDoExercicio("varres x : String");
+        when(tabelaDeSimbolos.getTipoSimbolo(solucaoDoUsuario.getSolucaoDoUsuario())).thenReturn("String");
+        when(tabelaDeSimbolos.getTipoSimbolo(possivelSolucao.getSolucaoDoUsuario())).thenReturn("Inteiro");
+
+        boolean resultado = validadorDeResultado.comparaTiposDoResultado(solucaoDoUsuario, possivelSolucao);
+
+        assertThat(resultado, is(false));
     }
 }
