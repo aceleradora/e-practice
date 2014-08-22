998520fa40db28cfd8695512a8bb9d06bc3bec7a
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-08-22 11:52:38 -0300

    2.2.1: Lucas & Leonardo - Padronização dos nomes dos testes

--- test/unitario/model/Arvore/NodoTest.java
+++ test/unitario/model/Arvore/NodoTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
-public class TesteNodo {
+public class NodoTest {
     private Nodo nodo;
 
     @Before
