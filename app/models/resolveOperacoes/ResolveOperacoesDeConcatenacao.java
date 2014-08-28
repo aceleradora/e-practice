7d3ca3d45fefb2af385f306f2f98b13686b80efa
filipegorges <lipirr@hotmail.com>
2014-08-28 15:42:47 -0300

    2.2.1: Aline & Luiz - Implementado o método que resolve operações de concatenação

--- app/models/resolveOperacoes/ResolveOperacoesDeConcatenacao.java
+++ app/models/resolveOperacoes/ResolveOperacoesDeConcatenacao.java
@@ -1,4 +1,33 @@
 package models.resolveOperacoes;
 
+import models.TabelaDeSimbolos;
+import models.analisadorLexico.IdentificadorDeToken;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ResolveOperacoesDeConcatenacao {
+    ArrayList<String> tokens;
+    IdentificadorDeToken identificadorDeToken;
+    TabelaDeSimbolos tabelaDeSimbolos;
+
+    public ResolveOperacoesDeConcatenacao() {
+        tokens = new ArrayList<String>();
+        identificadorDeToken = new IdentificadorDeToken();
+        tabelaDeSimbolos = new TabelaDeSimbolos();
+    }
+
+    public String concatenaStrings(ArrayList<String> tokens) {
+        String palavra = "";
+
+        for (int i = 0; i < tokens.size(); i++){
+            if (tabelaDeSimbolos.simboloExiste(tokens.get(i))){
+                palavra += tabelaDeSimbolos.getValor(tokens.get(i));
+            }
+            if (!identificadorDeToken.identifica(tokens.get(i)).equalsIgnoreCase("CONCATENACAO")){
+                palavra += tokens.get(i);
+            }
+        }
+        return palavra;
+    }
 }
--- test/unitario/model/resolveOperacoes/ResolveOperacoesDeConcatenacaoTeste.java
+++ test/unitario/model/resolveOperacoes/ResolveOperacoesDeConcatenacaoTeste.java
@@ -1,12 +1,78 @@
 package unitario.model.resolveOperacoes;
 
+import models.TabelaDeSimbolos;
+import models.resolveOperacoes.ResolveOperacoesDeConcatenacao;
+import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ResolveOperacoesDeConcatenacaoTeste {
+    TabelaDeSimbolos tabelaDeSimbolos;
+    ResolveOperacoesDeConcatenacao resolveOperacoesDeConcatenacao;
+
+    @Before
+    public void setUp() throws Exception {
+        tabelaDeSimbolos = new TabelaDeSimbolos();
+        resolveOperacoesDeConcatenacao = new ResolveOperacoesDeConcatenacao();
+    }
 
     @Test
-    public void dadoUmaOperacaoDeConcatenacaoComDuasStringRetornaUmaString() throws Exception {
+    public void dadoUmaOperacaoDeConcatenacaoComDuasStringsRetornaUmaString() throws Exception {
+        ArrayList<String> tokens = new ArrayList<String>();
+        tokens.add("casa ");
+        tokens.add("<>");
+        tokens.add("branca");
 
+        String result = resolveOperacoesDeConcatenacao.concatenaStrings(tokens);
 
+        assertThat(result, is("casa branca"));
+    }
+
+    @Test
+    public void dadoUmaOperacaoDeConcatenacaoComTresStringsRetornaUmaString() throws Exception {
+        ArrayList<String> tokens = new ArrayList<String>();
+        tokens.add("casa ");
+        tokens.add("<>");
+        tokens.add("branca ");
+        tokens.add("<>");
+        tokens.add("linda");
+
+        String result = resolveOperacoesDeConcatenacao.concatenaStrings(tokens);
+
+        assertThat(result, is("casa branca linda"));
+    }
+
+    @Test
+    public void dadoUmaOperacaoDeConcatenacaoComQuatroStringsRetornaUmaStringSemEspaco() throws Exception {
+        ArrayList<String> tokens = new ArrayList<String>();
+        tokens.add("casa");
+        tokens.add("<>");
+        tokens.add("branca");
+        tokens.add("<>");
+        tokens.add("linda");
+        tokens.add("<>");
+        tokens.add("linda");
+
+        String result = resolveOperacoesDeConcatenacao.concatenaStrings(tokens);
+
+        assertThat(result, is("casabrancalindalinda"));
+    }
+
+    @Test
+    public void dadoUmaOperacaoDeConcatenacaoDeUmaStringEUmaVariavelStringsRetornaUmaString() throws Exception {
+        ArrayList<String> tokens = new ArrayList<String>();
+        tabelaDeSimbolos.adicionaSimbolo("x", "String");
+        tabelaDeSimbolos.atualizaValor("x", "branca");
+
+        tokens.add("casa ");
+        tokens.add("<>");
+        tokens.add(tabelaDeSimbolos.getValor("x"));
+
+        String result = resolveOperacoesDeConcatenacao.concatenaStrings(tokens);
+
+        assertThat(result, is("casa branca"));
     }
 }
